075bbc976a894e075be42fb8970083656eb250a5
Ben Yellin <byellindc@gmail.com>
2019-01-08 18:03:28 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def name=(dog_breed)
+    @name = dog_breed
+  end
+ 
+  def name
+    @name
+  end
+
+  def job=(a_job)
+    @job = a_job
+  end
+ 
+  def job
+    return @job
+  end
+end
+
+# person = Person.new
+# person.name = "Dido"
+# person.job = "Mess-Maker"
+
